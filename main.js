6a03b809942aa3df701aebf97a3c93b138c2a4ac
RobsonGap <robsonmie@hotmail.com>
2020-01-14 20:55:53 -0300

    adicionando api do twitter -adicionando logica a function getQuote

--- main.js
+++ main.js
@@ -31,3 +31,27 @@ function getQuotes() {
 function getRandomQuote() {
     return quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
 }
+
+function getQuote() {
+
+    let randomQuote = getRandomQuote();
+
+    currentQuote = randomQuote.quote;
+    currentAuthor = randomQuote.author /
+
+        if (inIframe()) {
+            $('#tweet-quote').attr('href', 'https://twitter.com/internet/tweet?hashtags=quotes&realted=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '"'
+                currentAuthor));
+
+            $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' + encodeURIComponent(currentAuthor) + '&content=' + encodeURIComponent(currentQuote) + '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
+        }
+
+    $(".quote-text").animate({ opacity: 0 },
+        500,
+        function() {
+            $(this).animate({ opacity: 1 }, 500);
+            $('#author').html(randomQuote.author) /
+        }
+    );
+
+}
