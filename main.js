4af5edf2c2afbfb47f0d239395ad814aa2783f0a
RobsonGap <robsonmie@hotmail.com>
2020-01-14 20:37:36 -0300

    criando function retornando um ajax, function realizando getRandomQuote

--- main.js
+++ main.js
@@ -11,3 +11,23 @@ var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', 
 function openURL(url) {
     window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0,statusbar=0,menubar=0, resizable=0');
 }
+
+funtion getQuotes() {
+    return $.ajax({
+        headers: {
+            Accept: "applicationjson"
+        },
+        url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+        success: function(jsonQuotes) {
+            if (typeof jsonQuotes === 'string') {
+                quotesData = JSON.parse(jsonQuotes);
+                console.log('quotesData');
+                console.log(quotesData);
+            }
+        }
+    });
+}
+
+function getRandomQuote() {
+    return quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
+}
